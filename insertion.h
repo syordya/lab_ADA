55619fb0c3f802b2888930e1d3abf29d041aa7de
syordya <49656399+syordya@users.noreply.github.com>
2020-05-07 16:31:03 -0500

    Delete insertion.h

--- insertion.h
+++
@@ -1,8 +0,0 @@
-#include<algorithm>
-typedef double c_arr;
-void InsertSort(c_arr *A,const int p,const int n){
-	for(int i = p + 1; i < n; ++i)
-		for(int j = i; j > p; --j)
-			if(A[j-1] > A[j])
-				std::swap(A[j-1],A[j]);
-}
